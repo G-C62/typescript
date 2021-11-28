4e7b39dd4d3773b0dad58125dea25d26e3580fbf
G-C62 <nsw9211@gmail.com>
2021-11-28 15:33:49 +0900

    lecture:3-7 hacker news

--- hacker/app.js
+++ hacker/app.js
@@ -3,6 +3,9 @@ const ajax = new XMLHttpRequest();
 const content = document.createElement('div');
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
 const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
+const store = {
+    currentPage : 1
+};
 
 function getData(url) {
     ajax.open('GET', url, false);
@@ -14,26 +17,39 @@ function getData(url) {
 function newsFeed() {
     const newsFeed = getData(NEWS_URL);
     const newsList = [];
+    let template = `
+        <div>
+            <h1>Hacker News</h1>
+            <ul>
+                {{__news_feed__}}
+            </ul>
+            <div>
+                <a href="#/page/{{__prev_page__}}"> 이전 페이지 </a>
+                <a href="#/page/{{__next_page__}}"> 다음 페이지 </a>
+            </div>
+        </div>
+    `;
 
-    newsList.push('<ul>');
     
-    for(let i = 0; i < 10; i++){
+    for(let i = (store.currentPage - 1) * 10; i < store.currentPage * 10; i++){
         newsList.push(`
             <li>
-                <a href="#${newsFeed[i].id}">    
+                <a href="#/show/${newsFeed[i].id}">    
                     ${newsFeed[i].title} (${newsFeed[i].comments_count})
                 </a>
             </li>
         `);
     }
+
+    template = template.replace('{{__news_feed__}}', newsList.join(''));
+    template = template.replace('{{__prev_page__}}', store.currentPage > 1 ? store.currentPage - 1 : 1);
+    template = template.replace('{{__next_page__}}', newsFeed.length/10 <= store.currentPage ? store.currentPage : store.currentPage + 1);
     
-    newsList.push('</ul>');
-    
-    container.innerHTML = newsList.join('');
+    container.innerHTML = template;
 }
 
 function newsDetail() {
-    const id = location.hash.substr(1);
+    const id = location.hash.substr(7);
 
     const newsContent = getData(CONTENT_URL.replace('@id', id));
     const title = document.createElement('h1');
@@ -42,7 +58,7 @@ function newsDetail() {
         <h1>${newsContent.title}</h1>
 
         <div>
-            <a href='#'>목록으로</a>
+            <a href='#/page/${store.currentPage}'>목록으로</a>
         </div>
     `;
 
@@ -55,7 +71,12 @@ function router() {
 
     if(routePath === ''){
         newsFeed();
-    } else {
+    } 
+    else if(routePath.indexOf('#/page/') >= 0){
+        store.currentPage = Number(routePath.substr(7));
+        newsFeed();
+    }
+    else {
         newsDetail();
     }
 }
